e16942978c534108d600ed9d280e49f52155e366
VishnuRameshbabu <vishrathish@gmail.com>
2024-09-14 00:32:43 -0400

    two way channel for thread communication

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,88 @@
-    //use std::os::windows::thread;  // import stmts
-    use std::sync::{Arc, Mutex};
-    use std::sync::mpsc::{Sender,Receiver};
-    use std::thread::{self, Thread, ThreadId};
-    use std::collections::HashMap;
+use std::sync::{Arc, Mutex, mpsc::{Sender, Receiver, channel}};
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
 
+#[derive(Debug, Clone)]
+pub struct BatteryInformation {
+    threadId: thread::ThreadId,
+    command: String,
+    threadName: u32,
+    sender: Sender<thread::ThreadId>,   // senderMainToThread, the main loop uses this to send information to the specific thread.
+counter:u32}
 
-    #[derive(Debug,Clone)]
-    pub struct batteryInformation{
-            threadId: ThreadId,
-            command:String,
-    }   // a mock struct for sending data
+fn main() {
+    static NUMBERS: &'static [u32] = &[100,101, 102, 103, 104, 105, 106, 107, 108, 109,999];            // assuming these are batteries
+    let threadIdMapCommon = Arc::new(Mutex::new(HashMap::<thread::ThreadId, BatteryInformation>::new()));       // a common Thread map to have the battery information for each battery thread.
+    let (senderThreadToMain, receiverThreadToMain): (Sender<BatteryInformation>, Receiver<BatteryInformation>) = channel(); // a channel to send data from thread loop to main loop
+    let mut threadHandles = vec![];
+    for &num in NUMBERS.iter() {        // thread For loop
+        let senderMainClone = senderThreadToMain.clone();       
+        let threadIdMap = Arc::clone(&threadIdMapCommon);
+        let (senderMainToThread, receiverMainToThread): (Sender<thread::ThreadId>, Receiver<thread::ThreadId>) = channel(); // creating an individual channel for each thread between main and to the specific thread.
+        let handle = thread::spawn(move || {
+            let threadId = thread::current().id();
+            let mut counter = 0;
+            loop {      // thread loop
+                let mut batteryInformation:BatteryInformation;
 
-    
-    fn main(){
-    let mut threadMap:HashMap<ThreadId,batteryInformation>=HashMap::new();
-    static numbers: &'static [u32] = &[0,1,2,3,4,5,6,7,8,9];
+                if let Ok(receivingThreadId) = receiverMainToThread.try_recv() {        // receives information from the main loop
+                        let mut threadMap = threadIdMap.lock().unwrap();
+                    println!("Thread Loop - Thread {:?} received battery Information Update from main loop: {:?}", threadId, threadMap.get(&receivingThreadId));
+                }
+                let mut counterString:String = counter.to_string();
+                if counter == 5{
+                        batteryInformation = BatteryInformation {
+                           threadId,
+                           command: String::from("Battery status COUNTER REACHED ") + &counterString,
+                           threadName: num,
+                           sender: senderMainToThread.clone(), 
+                           counter: counter
+                       };
+               } else{
+                       batteryInformation = BatteryInformation {
+                               threadId,
+                               command: String::from("Battery status OK ") + &counterString,
+                               threadName: num,
+                               sender: senderMainToThread.clone(), 
+                               counter:counter
+                           };
+               }
+                senderMainClone.send(batteryInformation.clone()).unwrap();
+                {
+                    let mut threadMap = threadIdMap.lock().unwrap();
+                    threadMap.insert(threadId, batteryInformation.clone());
+                }
+                thread::sleep(Duration::from_secs(2));
+                counter += 1;
 
-
-
-    //loop{
-    
-    let mut ThreadIdArray = Arc::new(Mutex::new(Vec::<ThreadId>::new()));
-    let (senderThread, receiverThread):(Sender<batteryInformation>,Receiver<batteryInformation>) = std::sync::mpsc::channel();
-    for i in numbers.iter(){
-    let senderSpace: Sender<batteryInformation> = senderThread.clone();
-    let ThreadIdArray = Arc::clone(&ThreadIdArray);
-   
-   //As of now, trying to send data in unidirection and modifying it. 
-    thread::spawn(move || {
-            println!("{:?}",thread::current().id());
-            let mut threadInfo = batteryInformation{threadId:thread::current().id(),command:String::from("test")};
-            ThreadIdArray.lock().unwrap().push(threadInfo.threadId);
-            senderSpace.send(threadInfo).unwrap();
-           
+            }
             
-    });
-    let receivingBatteryInformation = receiverThread.recv().unwrap();
-    threadMap.insert(receivingBatteryInformation.threadId,receivingBatteryInformation);
+        });
+        threadHandles.push(handle);
     }
 
+    loop {
 
-    let ThreadIdArray = ThreadIdArray.lock().unwrap();
-    println!("Received messages queue of Threads:{:?}",threadMap);
-    println!("First thread id : {:?}",ThreadIdArray[0]);
-    threadMap.get_mut(&ThreadIdArray[0]).unwrap().command = String::from("changed");
-    println!("Received messages Id :{:?} , and command :{:?}",threadMap.get(&ThreadIdArray[0]).unwrap().threadId,threadMap.get(&ThreadIdArray[0]).unwrap().command);
-    
+        if let Ok(receivingBatteryInformation) = receiverThreadToMain.recv() {          // this receives the informatio from the thread.
+            println!("Main Loop received: {:?}", receivingBatteryInformation);
+            let threadId = receivingBatteryInformation.threadId;
+            let mut threadMap = threadIdMapCommon.lock().unwrap();
+            if let Some(info) = threadMap.get_mut(&threadId) {
+               
+                if info.command == "Battery status COUNTER REACHED" {
+                
+                let mut message:String = String::from("COUNTER REACHED at 5 on Receiving End.. Modifying counter - ") + &(String::from(info.counter.to_string()));
+                    info.command = message;
+                } else {
+                    info.command = String::from("Main Loop - Changing information counter - ") + &(String::from(info.counter.to_string()));
+                }
+            }
+
+            println!("Main Loop -Updated battery  info for Thread ID to be sent {:?}: {:?}", threadId, threadMap.get(&threadId));
+
+            receivingBatteryInformation.sender.send(threadId).unwrap();
+        }
     }
 
-//  }
+}
